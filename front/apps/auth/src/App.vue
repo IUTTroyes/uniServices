530c143c9b20993e03434143c4126257d957f92d
davidannebicque <david.annebicque@gmail.com>
2024-11-08 13:56:21 +0100

    Mise en place de la structure front

---
+++ front/apps/auth/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>App 1</h1>
+  <div>
+    {{ formattedDate }}
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { formatDate } from 'common-helpers';
+
+const formattedDate = ref('');
+
+onMounted(() => {
+  formattedDate.value = formatDate(new Date());
+});
+</script>
