46663fe9a8d8cd0398d418f656d77329f49c8a29
davidannebicque <david.annebicque@gmail.com>
2024-11-09 18:20:18 +0100

    Configuration de prime vue avec autoload

--- front/apps/auth/src/App.vue
+++ front/apps/auth/src/App.vue
@@ -2,6 +2,8 @@
   <h1>App 1</h1>
   <div>
     {{ formattedDate }}
+    <InputText v-model="text" type="text" />
+    {{ text }}
   </div>
 </template>
 
@@ -10,6 +12,7 @@ import { ref, onMounted } from 'vue';
 import { formatDate } from 'common-helpers';
 
 const formattedDate = ref('');
+const text = ref('');
 
 onMounted(() => {
   formattedDate.value = formatDate(new Date());
--- front/apps/intranet/vite.config.js
+++ front/apps/intranet/vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { PrimeVueResolver } from '@primevue/auto-import-resolver';
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,8 +8,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
-  ],
+    Components({
+      resolvers: [
+        PrimeVueResolver()
+      ]
+    })],
+  base: '/intranet/', // Base pour app2
   server: {
+    host: 'iutranet.fr', //
     port: 3001 // Port pour app2
   },
   resolve: {
