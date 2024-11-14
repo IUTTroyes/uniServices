719bf2e4c8035b70a011a00345a823283cd5b3bb
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-11-14 15:47:58 +0100

    feat: import and config - template

--- front/apps/intranet/src/main.js
+++ front/apps/intranet/src/main.js
@@ -1,19 +1,27 @@
-import PrimeVue from "primevue/config";
-import Aura from '@primevue/themes/aura';
-import 'common-styles/main.scss';
-
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import Aura from '@primevue/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+import '@/assets/styles.scss';
+import '@/assets/tailwind.css';
 
 const app = createApp(App);
-app.use(PrimeVue, {
-    theme: {
-        preset: Aura
-    }
-});
 
 app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
 
-app.mount('#app')
+app.mount('#app');
