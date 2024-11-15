3274d47a2c4c6e33c96cd08bc7618c92a0e4d4b3
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-11-15 14:59:41 +0100

    feat: mise en commun des composants Vue + première refonte du layout

---
+++ front/packages/common-components/index.js
@@ -0,0 +1,15 @@
+import LayoutComponent from './components/AppLayout.vue';
+import ConfiguratorComponent from './components/AppConfigurator.vue';
+import FooterComponent from './components/AppFooter.vue';
+import MenuComponent from './components/AppMenu.vue';
+import MenuItemComponent from './components/AppMenuItem.vue';
+import SidebarComponent from './components/AppSidebar.vue';
+import TopbarComponent from './components/AppTopbar.vue';
+
+export { LayoutComponent };
+export { ConfiguratorComponent };
+export { FooterComponent };
+export { MenuComponent };
+export { MenuItemComponent };
+export { SidebarComponent };
+export { TopbarComponent };
