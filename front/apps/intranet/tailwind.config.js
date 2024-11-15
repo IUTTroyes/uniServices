e7f6344569e3c0d1045594554d576a66d0a41e0f
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-11-15 11:27:01 +0100

    feat: mise en commun de la config front

--- front/apps/intranet/tailwind.config.js
+++ front/apps/intranet/tailwind.config.js
@@ -1,15 +1,1 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    darkMode: ['selector', '[class*="app-dark"]'],
-    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-    plugins: [require('tailwindcss-primeui')],
-    theme: {
-        screens: {
-            sm: '576px',
-            md: '768px',
-            lg: '992px',
-            xl: '1200px',
-            '2xl': '1920px'
-        }
-    }
-};
+module.exports = require('../../packages/common-config/tailwind.config.js');
