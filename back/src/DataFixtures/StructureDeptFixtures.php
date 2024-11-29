8a1406751b1349b7f91f9c1085d3e185ce0d4cf6
davidannebicque <david.annebicque@gmail.com>
2024-11-29 16:12:26 +0100

    [WIP][back] : Corrections fixtures

--- back/src/DataFixtures/StructureDeptFixtures.php
+++ back/src/DataFixtures/StructureDeptFixtures.php
@@ -8,6 +8,7 @@ use App\Repository\PersonnelRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Uid\UuidV4;
 
 class StructureDeptFixtures extends Fixture implements OrderedFixtureInterface
 {
@@ -32,7 +33,8 @@ class StructureDeptFixtures extends Fixture implements OrderedFixtureInterface
             ->setCouleur('#0096ff')
             ->setSiteWeb('https://mmi.iut-troyes.univ-reims.fr/but-mmi/')
             ->setDescription('Le département Métiers de l’Internet et du Multimédia de l’IUT de Troyes (précédemment Services et Réseaux de Communication) forme les acteurs de l’Internet, des médias numériques, de la communication plurimédia, de la création.')
-            ->setActif(true);
+            ->setActif(true)
+            ->setUuid(new UuidV4());
         $manager->persist($departement1);
 
         $departement2 = new StructureDepartement();
@@ -42,7 +44,8 @@ class StructureDeptFixtures extends Fixture implements OrderedFixtureInterface
             ->setCouleur('#007861')
             ->setSiteWeb(null)
             ->setDescription('La gestion des entreprises et des administrations représente à la fois le coeur et l’esprit de toute structure humaine dont l’objectif est de proposer un service à un public. Le coeur, car au fondement de toute organisation se trouvent des hommes et des femmes, et par conséquent un besoin de les diriger. L’esprit, car au sein de cet ensemble dynamique circulent des données et des flux financiers, et par conséquent un besoin de les comprendre et de les manipuler. Discipline immuable, la gestion des entreprises et des administrations ne représente un savoir-faire vivant : elle évolue avec le monde dans lequel elle s’inscrit.')
-            ->setActif(true);
+            ->setActif(true)
+            ->setUuid(new UuidV4());
         $manager->persist($departement2);
 
         $personnel = $this->personnelRepository->findOneBy(['username' => 'hero0010']);
