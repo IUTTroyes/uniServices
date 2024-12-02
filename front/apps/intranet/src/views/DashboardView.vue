154b6e031e05829e6b9f1f5b580faca4b5da82ba
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-12-02 10:29:27 +0100

    fix: [Intranet] fetch user data après la connexion

--- front/apps/intranet/src/views/DashboardView.vue
+++ front/apps/intranet/src/views/DashboardView.vue
@@ -21,6 +21,8 @@ const date = new Date().toLocaleDateString('fr-FR', {
 const initiales = computed(() =>
     store.user?.prenom?.charAt(0) + store.user?.nom?.charAt(0) || ''
 );
+
+console.log(store.user);
 </script>
 
 <template>
@@ -29,15 +31,15 @@ const initiales = computed(() =>
             <div class="flex items-center justify-between">
                 <div class="flex items-center">
                     <div class="w-16 bg-gray-200 rounded-full flex items-center justify-center">
-                        <template v-if="store.user?.photo_name">
-                            <img :src="store.user.photo_name" alt="photo de profil" class="rounded-full">
+                        <template v-if="store.user.photoName">
+                            <img :src="store.user.photoName" alt="photo de profil" class="rounded-full">
                         </template>
                         <template v-else>
                             <span class="text-gray-700 text-xl">{{ initiales }}</span>
                         </template>
                     </div>
                     <div class="ml-4">
-                        <h1 class="text-2xl font-semibold"><span class="font-light">Bonjour,</span> {{ store.user?.prenom }}</h1>
+                        <h1 class="text-2xl font-semibold"><span class="font-light">Bonjour,</span> {{ store.user.prenom }}</h1>
                         <small>{{ formatDateLong(date) }}</small>
                     </div>
                 </div>
