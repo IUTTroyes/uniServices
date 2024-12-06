514ca5cd86503bc4e1bd759101c2fd30a5d943ee
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-12-06 14:53:13 +0100

    wip [intranet] : edt structure vue semestre

--- front/apps/intranet/src/components/Edt/EdtSemestre.vue
+++ front/apps/intranet/src/components/Edt/EdtSemestre.vue
@@ -12,15 +12,36 @@ watch(() => props.data.days, (newDays) => {
     days.value = newDays;
     const currentDayExists = newDays.some(day => day.dayNumber === currentDay.value);
 });
+
+const semestres = [
+    { name: 'Semestre 1', value: '1' },
+    { name: 'Semestre 2', value: '2' },
+    { name: 'Semestre 3', value: '3' },
+    { name: 'Semestre 4', value: '4' },
+    { name: 'Semestre 5', value: '5' },
+    { name: 'Semestre 6', value: '6' }
+];
+
+const semaines = [
+    { name: 'Semaine 1', value: '1' },
+    { name: 'Semaine 2', value: '2' },
+    { name: 'Semaine 3', value: '3' },
+    { name: 'Semaine 4', value: '4' },
+    { name: 'Semaine 5', value: '5' },
+    { name: 'Semaine 6', value: '6' }
+];
 </script>
 
 <template>
+    <div class="flex gap-4">
+        <Select v-model="selectedCity" :options="semestres" optionLabel="name" placeholder="Choisir un semestre" class="w-full md:w-56" />
+        <Select v-model="selectedCity" :options="semaines" optionLabel="name" placeholder="Choisir une semaine" class="w-full md:w-56" />
+    </div>
 
     <Carousel :value="days" :numVisible="1" :numScroll="1">
         <template #item="slotProps">
-            <div class="bg-gray-200 bg-opacity-20 rounded-md">
-                <div
-                    :class="['day text-center uppercase font-bold flex flex-col p-4', { 'bg-primary': currentDay === slotProps.data.dayNumber, active: currentDay === slotProps.data.dayNumber }]">
+            <div class="bg-gray-200 bg-opacity-20 rounded-md m-3">
+                <div :class="['day text-center uppercase font-bold flex flex-col p-4', { 'bg-primary': currentDay === slotProps.data.dayNumber, active: currentDay === slotProps.data.dayNumber }]">
                     {{ slotProps.data.dayName }} <span class="font-black">{{ slotProps.data.dayNumber }}</span>
                 </div>
             </div>
