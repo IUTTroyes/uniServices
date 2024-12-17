7c7ba334799799ae69486e3ae60c4040bfc7e511
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-12-17 17:13:39 +0100

    wip [intranet] : admin + edt

---
+++ front/apps/intranet/src/components/Edt/EdtJour.vue
@@ -0,0 +1,54 @@
+<script setup>
+// requête 3 cours à venir
+const events = [
+    {
+        debut: '10/12/2024 08:00',
+        fin: '10/12/2024 09:30',
+        text: 'WS301D - Développer des parcours utilisateur au sein d\'un système d\'information',
+        color: '#FBE4EE',
+        colorFocus: '#fbc6e3',
+        semestre: 'S3 Strat-UX Alt',
+        groupe: 'CM',
+        groupeColor: '#1f4ea6',
+        salle: 'H018'
+    },
+    {
+        debut: '10/12/2024 09:30',
+        fin: '10/12/2024 11:00',
+        text: 'WS301D - Développer des parcours utilisateur au sein d\'un système d\'information',
+        color: '#FBE4EE',
+        colorFocus: '#fbc6e3',
+        semestre: 'S3 Strat-UX Alt',
+        groupe: 'CM',
+        groupeColor: '#1f4ea6',
+        salle: 'H018'
+    }
+]
+</script>
+
+<template>
+    <div class="flex flex-row justify-between gap-4">
+        <div v-for="(event, index) in events" class="w-full">
+            <div class="flex flex-row justify-between items-center p-4 bg-white rounded-md">
+                <div class="flex flex-col">
+                    <div class="text-sm text-gray-500">{{ event.debut }} - {{ event.fin }}</div>
+                    <div class="text-lg font-semibold">{{ event.text }}</div>
+                    <div class="text-sm text-gray-500">{{ event.semestre }}</div>
+                </div>
+                <div class="flex flex-col items-end">
+                    <div class="flex flex-row items-center">
+                        <div class="w-4 h-4 rounded-full bg-gray-200"></div>
+                        <div class="text-sm text-gray-500 ml-2">{{ event.groupe }}</div>
+                    </div>
+                    <div class="flex flex-row items-center">
+                        <div class="w-4 h-4 rounded-full bg-gray-200"></div>
+                        <div class="text-sm text-gray-500 ml-2">{{ event.salle }}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+
+</style>
--- front/apps/intranet/src/router/modules/administrationRoutes.js
+++ front/apps/intranet/src/router/modules/administrationRoutes.js
@@ -5,5 +5,10 @@ export default [
         path: 'administration',
         component: () => import('@/views/AdministrationView.vue'),
         meta: { breadcrumb: [{ label: 'Dashboard', route: '/' }, { label: 'Administration', route: null, icon: 'pi pi-wrench' }] },
+    },
+    {
+        path: 'administration/pn',
+        component: () => import('@/components/administration/Pn.vue'),
+        meta: { breadcrumb: [{ label: 'Dashboard', route: '/' }, { label: 'Administration', route: '/administration' }, { label: 'PN', route: null }] },
     }
 ];
