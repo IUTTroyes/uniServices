bc6d95bab2b2a672ff38a400268783e6e25b9c8d
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2024-12-18 11:12:50 +0100

    wip [intranet] : affichage rapide des deux prochain cours

--- front/apps/intranet/src/components/Edt/EdtJour.vue
+++ front/apps/intranet/src/components/Edt/EdtJour.vue
@@ -3,17 +3,17 @@ import { ref } from 'vue';
 
 // todo: requête pour récupérer l'evenement en cours et le suivant - si pas d'event en cours, on ne récupère que le suivant
 const events = ref([
-    // {
-    //     debut: '2024-12-18T10:00:00',
-    //     fin: '2024-12-18T11:30:00',
-    //     text: 'WS301D - Développer des parcours utilisateur au sein d\'un système d\'information',
-    //     color: '#FFEDD2',
-    //     colorFocus: '#ffdeae',
-    //     semestre: 'S3 Strat-UX Alt',
-    //     groupe: 'TD GH',
-    //     groupeColor: '#1f4ea6',
-    //     salle: 'H018'
-    // },
+    {
+        debut: '2024-12-18T10:00:00',
+        fin: '2024-12-18T11:30:00',
+        text: 'WS301D - Développer des parcours utilisateur au sein d\'un système d\'information',
+        color: '#FFEDD2',
+        colorFocus: '#ffdeae',
+        semestre: 'S3 Strat-UX Alt',
+        groupe: 'TD GH',
+        groupeColor: '#1f4ea6',
+        salle: 'H018'
+    },
     {
         debut: '2024-12-10T09:30:00',
         fin: '2024-12-10T11:00:00',
@@ -65,7 +65,7 @@ const isToday = (dateString) => {
                 :class="['flex flex-row justify-between items-start rounded-lg h-full relative text-black', {'border-4': index === 0 && event.text !== 'Aucun cours'}, {'text-color palm': event.text === 'Aucun cours'}]"
                 :style="{ borderColor: event.colorFocus, backgroundColor: event.color }">
                 <div v-if="event.text !== 'Aucun cours'" class="flex flex-col justify-center items-center p-4 rounded-l-md h-full" :style="{ backgroundColor: event.colorFocus }">
-                    <div v-if="!isToday(event.debut)" class="text-sm font-bold">{{ new Date(event.debut).toLocaleDateString('fr-FR') }}</div>
+                    <div v-if="!isToday(event.debut)" class="text-sm font-bold flex flex-col items-center"><span>{{ new Date(event.debut).toLocaleDateString('fr-FR', {weekday: 'long'}) }}</span><span>{{ new Date(event.debut).toLocaleDateString('fr-FR') }}</span></div>
                     <div v-else class="text-sm font-bold">Aujourd'hui</div>
                     <div class="text-xl font-black">{{ event.debut.split('T')[1].slice(0, 5) }}</div>
                     <div class="text-xl font-black">{{ event.fin.split('T')[1].slice(0, 5) }}</div>
