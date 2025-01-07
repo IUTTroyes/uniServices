7a790730258c4495302bb0a0c8ffca398614ac7f
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-07 19:51:26 +0100

    wip [tests] : cypress CI

--- front/cypress/e2e/login.cy.js
+++ front/cypress/e2e/login.cy.js
@@ -1,17 +1,23 @@
 describe('Login Test', () => {
-  it('Visits UniServices and logs in as a guest', () => {
-    cy.visit('http://localhost:3000')
+  it('Logs in as a guest', () => {
+    cy.visit('/auth/login')
 
-    cy.contains('Connexion invité').click()
-
-    // Remplir les champs
-    cy.get('#username').type('hero0010')
-    cy.get('#password').type('test')
-
+    cy.get('#username').type('guest')
+    cy.get('#password').type('guestpassword')
     cy.get('form').submit()
 
-    // Vérifier la réussite de la connexion et la redirection
+    cy.intercept('POST', '/api/login').as('loginRequest')
+
+    cy.wait('@loginRequest').then((interception) => {
+      assert.isNotNull(interception.response.body, 'Login API call has data')
+      expect(interception.response.statusCode).to.eq(200)
+      expect(interception.response.body).to.have.property('data')
+      expect(interception.response.body.data).to.have.property('token')
+
+      const token = interception.response.body.data.token
+      cy.setCookie('token', token)
+    })
+
     cy.url().should('include', '/auth/portail')
-    cy.getCookie('token').should('exist')
   })
 })
