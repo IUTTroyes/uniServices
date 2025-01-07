2a295cc1518891be0a433378797c59adb50da365
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-07 17:02:00 +0100

    wip [tests] : cypress CI

--- front/cypress/e2e/login.cy.js
+++ front/cypress/e2e/login.cy.js
@@ -1,30 +1,16 @@
 describe('Login Test', () => {
-  beforeEach(() => {
+  it('Visits UniServices and logs in as a guest', () => {
     cy.visit('http://localhost:3000')
-  })
 
-  it('Logs in as a guest', () => {
-    // Vérifier si on est sur Cypress
-    // if (window.Cypress) {
-    //
-    //   // Intercepter la requête de login et renvoyer un fixture
-    //   cy.intercept('POST', 'https://localhost:8000/api/login', { fixture: 'login.json' }).as('loginRequest')
-    // }
+    cy.contains('Connexion invité').click()
 
-    cy.contains('Connexion invité').should('be.visible').click()
-
-    // Remplir les champs de connexion
-    cy.get('#username').should('be.visible').type('hero0010')
-    cy.get('#password').should('be.visible').type('test')
+    // Remplir les champs
+    cy.get('#username').type('hero0010')
+    cy.get('#password').type('test')
 
     cy.get('form').submit()
 
-    // Attendre la fin de la requête de login
-    // if (window.Cypress) {
-    //   cy.wait('@loginRequest').its('response.statusCode').should('eq', 200)
-    // }
-
-    // Vérifier si on est redirigé vers la page de portail
+    // Vérifier la réussite de la connexion et la redirection
     cy.url().should('include', '/auth/portail')
     cy.getCookie('token').should('exist')
   })
