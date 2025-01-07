5350e9b6dca37b3a0bfa83d764e44bfa53dbbace
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-07 11:42:39 +0100

    fix [tests] : cypress login -> n'utiliser la fixture que si on est dans un environnement cypress

--- front/cypress/e2e/login.cy.js
+++ front/cypress/e2e/login.cy.js
@@ -4,21 +4,26 @@ describe('Login Test', () => {
   })
 
   it('Logs in as a guest', () => {
+    // Vérifier si on est sur Cypress
+    if (window.Cypress) {
+      // Intercepter la requête de login et renvoyer un fixture
+      cy.intercept('POST', 'https://localhost:8000/api/login', { fixture: 'login.json' }).as('loginRequest')
+    }
+
     cy.contains('Connexion invité').should('be.visible').click()
 
-    // intercepte la requête POST vers /api/login et renvoie un fichier JSON
-    cy.intercept('POST', 'https://localhost:8000/api/login', { fixture: 'login.json' }).as('loginRequest')
-
-    // renseigne les champs du formulaire
+    // Remplir les champs de connexion
     cy.get('#username').should('be.visible').type('user')
     cy.get('#password').should('be.visible').type('test')
 
     cy.get('form').submit()
 
-    // Attend la fin de la requête
-    cy.wait('@loginRequest').its('response.statusCode').should('eq', 200)
+    // Attendre la fin de la requête de login
+    if (window.Cypress) {
+      cy.wait('@loginRequest').its('response.statusCode').should('eq', 200)
+    }
 
-    // Vérifie que l'utilisateur est bien connecté
+    // Vérifier si on est redirigé vers la page de portail
     cy.url().should('include', '/auth/portail')
     cy.getCookie('token').should('exist')
   })
