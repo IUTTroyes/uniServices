123fca1af033533aabfe7ca6afa7a678abdf1278
davidannebicque <david.annebicque@gmail.com>
2025-01-09 21:25:11 +0100

    [WIP][UniEdt][back] : Synchro avec ORéOF

---
+++ back/src/Services/OReOF/SynchroParcours.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services\OReOF;
+
+class SynchroParcours
+{
+    public function __construct()
+    {
+    }
+
+    public function sync(int $parcoursId)
+    {
+        // Logique pour la synchronisation d'un parcours
+    }
+}
