3a75fd8974c08bdc87b332acc6c8cbe5662e9e60
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-09 09:11:40 +0100

    wip [tests] : cypress CI

--- front/cypress.config.js
+++ front/cypress.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "fviiku",
+  env: {
+    VITE_BASE_URL: process.env.VITE_BASE_URL,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
