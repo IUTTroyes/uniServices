1d1290bab279ccb4e32d72dc08cdf179418fd559
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-09 09:39:30 +0100

    wip [tests] : cypress CI

--- front/cypress.config.js
+++ front/cypress.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+import { resolve } from 'path';
+
+// Charger les variables d'environnement à partir du fichier .env
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 export default defineConfig({
-  projectId: "fviiku",
+  projectId: 'fviiku',
+  env: {
+    VITE_BASE_URL: process.env.VITE_BASE_URL,
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- front/packages/common-helpers/axios.js
+++ front/packages/common-helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 console.log(import.meta.env.VITE_BASE_URL);
-const baseURL = import.meta.env.VITE_BASE_URL || 'https://127.0.0.1:8000';
+const baseURL = import.meta.env.VITE_BASE_URL;
 const api = axios.create({
     baseURL
 });
