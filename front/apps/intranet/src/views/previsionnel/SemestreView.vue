6001893ebef56e72ddfd35942477d9b1f9a88911
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-01-18 16:25:14 +0100

    wip: refactor récupération des semestres du dept

--- front/apps/intranet/src/views/previsionnel/SemestreView.vue
+++ front/apps/intranet/src/views/previsionnel/SemestreView.vue
@@ -1,8 +1,20 @@
 <script setup>
+import {useSemestreStore} from "@stores";
+import {onMounted, ref} from "vue";
+
+const semestreStore = useSemestreStore();
+const departementId = localStorage.getItem('departement');
+const semestres = ref([]);
+
+onMounted(async () => {
+  await semestreStore.getSemestresByDepartement(departementId, );
+  semestres.value = semestreStore.semestres;
+  console.log(semestres.value);
+});
 </script>
 
 <template>
-  <div>par semestre</div>
+  <div v-for="semestre in semestres">{{semestre.libelle}}</div>
 </template>
 
 <style scoped>
