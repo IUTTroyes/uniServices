e1ef2c39c4cbf7409c9e656f9be5ed62c5402d97
davidannebicque <david.annebicque@gmail.com>
2025-02-09 16:14:11 +0100

    [WIP][front][personnel] : Page personnel, avec boutons colonnes et modals

--- front/apps/intranet/src/App.vue
+++ front/apps/intranet/src/App.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-    <router-view />
+  <ConfirmDialog></ConfirmDialog>
+
+  <router-view />
 </template>
 
 <style scoped></style>
---
+++ front/packages/common-components/components/ButtonEdit.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+const props = defineProps({
+  'tooltip': {
+    type: String,
+    required: true
+  },
+})
+</script>
+
+<template>
+  <Button icon="pi pi-pencil"
+          outlined
+          severity="warn"
+          rounded
+          v-tooltip.bottom="tooltip"
+          class="mr-2"
+           />
+</template>
+
+<style scoped>
+
+</style>
