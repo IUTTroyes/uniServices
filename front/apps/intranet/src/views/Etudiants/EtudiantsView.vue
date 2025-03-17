2aec4d405e37c71433adb381dd2988c8e97264ef
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-03-17 14:06:06 +0100

    fix[administration]: affichage de tous les étudiants
    un seul envoi de la requête + correction de la props passé aux dialogs

--- front/apps/intranet/src/views/Etudiants/EtudiantsView.vue
+++ front/apps/intranet/src/views/Etudiants/EtudiantsView.vue
@@ -1,119 +1,115 @@
 <script setup>
-      import { ref, onMounted, computed, watch } from 'vue'
-      import { FilterMatchMode } from '@primevue/core/api'
-      import api from '@helpers/axios.js'
-      import ButtonInfo from '@components/components/ButtonInfo.vue'
-      import ButtonEdit from '@components/components/ButtonEdit.vue'
-      import ButtonDelete from '@components/components/ButtonDelete.vue'
+import { ref, onMounted, computed, watch } from 'vue'
+import { FilterMatchMode } from '@primevue/core/api'
+import api from '@helpers/axios.js'
+import ButtonInfo from '@components/components/ButtonInfo.vue'
+import ButtonEdit from '@components/components/ButtonEdit.vue'
+import ButtonDelete from '@components/components/ButtonDelete.vue'
 
-      import ViewEtudiantDialog from '@/dialogs/etudiants/ViewEtudiantDialog.vue'
-      import EditEtudiantDialog from '@/dialogs/etudiants/EditEtudiantDialog.vue'
-      import AccessEtudiantDialog from '@/dialogs/etudiants/AccessEtudiantDialog.vue'
+import ViewEtudiantDialog from '@/dialogs/etudiants/ViewEtudiantDialog.vue'
+import EditEtudiantDialog from '@/dialogs/etudiants/EditEtudiantDialog.vue'
+import AccessEtudiantDialog from '@/dialogs/etudiants/AccessEtudiantDialog.vue'
 
-      import { getEtudiantsDepartementService } from "@requests";
+import { getEtudiantsDepartementService } from "@requests";
 
-      import {useAnneeUnivStore, useUsersStore} from "@stores";
-      import {SimpleSkeleton} from "@components";
-      const usersStore = useUsersStore();
-      const anneeUnivStore = useAnneeUnivStore();
+import { useAnneeUnivStore, useUsersStore } from "@stores";
+import { SimpleSkeleton } from "@components";
+const usersStore = useUsersStore();
+const anneeUnivStore = useAnneeUnivStore();
 
-      const departementId = ref(null);
-      const anneesUnivList = ref([]);
-      const selectedAnneeUniv = ref(null);
+const departementId = ref(null);
+const anneesUnivList = ref([]);
+const selectedAnneeUniv = ref(null);
 
-      const isLoadingAnneesUniv = ref(false);
+const isLoadingAnneesUniv = ref(false);
 
-      const etudiants = ref([])
-      const nbEtudiants = ref(0)
-      const loading = ref(true)
-      const page = ref(0)
-      const rowOptions = [30, 60, 120]
+const etudiants = ref([])
+const nbEtudiants = ref(0)
+const loading = ref(true)
+const page = ref(0)
+const rowOptions = [30, 60, 120]
 
-      const limit = ref(rowOptions[0])
-      const offset = computed(() => limit.value * page.value)
+const limit = ref(rowOptions[0])
+const offset = computed(() => limit.value * page.value)
 
-      const filters = ref({
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        nom: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        prenom: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        mailUniv: { value: null, matchMode: FilterMatchMode.EQUALS },
-      })
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  nom: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  prenom: { value: null, matchMode: FilterMatchMode.CONTAINS },
+  mailUniv: { value: null, matchMode: FilterMatchMode.EQUALS },
+})
 
-      const showViewDialog = ref(false)
-      const showEditDialog = ref(false)
-      const showAccessEditDialog = ref(false)
-      const selectedEtudiant = ref(null)
+const showViewDialog = ref(false)
+const showEditDialog = ref(false)
+const showAccessEditDialog = ref(false)
+const selectedEtudiant = ref(null)
 
-      const getAnneesUniv = async () => {
-        isLoadingAnneesUniv.value = true;
-        await anneeUnivStore.getAllAnneesUniv();
-        anneesUnivList.value = anneeUnivStore.anneesUniv.sort((a, b) => b.id - a.id);
-        await anneeUnivStore.getCurrentAnneeUniv();
-        selectedAnneeUniv.value = anneeUnivStore.anneeUniv;
-        isLoadingAnneesUniv.value = false;
-      };
+const getAnneesUniv = async () => {
+  isLoadingAnneesUniv.value = true;
+  await anneeUnivStore.getAllAnneesUniv();
+  anneesUnivList.value = anneeUnivStore.anneesUniv.sort((a, b) => b.id - a.id);
+  await anneeUnivStore.getCurrentAnneeUniv();
+  selectedAnneeUniv.value = anneeUnivStore.anneeUniv;
+  isLoadingAnneesUniv.value = false;
+};
 
-      const loadEtudiants = async () => {
-        loading.value = true
-        const response = await getEtudiantsDepartementService(departementId.value, selectedAnneeUniv.value.id, limit.value, parseInt(page.value) + 1)
-        etudiants.value = response.member
-        nbEtudiants.value = response.totalItems
-        loading.value = false
+const loadEtudiants = async () => {
+  loading.value = true
+  const response = await getEtudiantsDepartementService(departementId.value, selectedAnneeUniv.value.id, limit.value, parseInt(page.value) + 1)
+  etudiants.value = response.member
+  nbEtudiants.value = response.totalItems
+  loading.value = false
 
-        // récupérer l'etudiantScolarite parmi les etudiantScolarites de l'etudiant celle dont l'anneeUniv est celle selectionnée
-        // l'ajouter à l'etudiant pour l'afficher dans le tableau
-        etudiants.value.forEach(etudiant => {
-          etudiant.etudiantScolarite = etudiant.etudiantScolarites.find(es => es.structureAnneeUniversitaire.id === selectedAnneeUniv.value.id)
-        })
-        // ajouter tous les etudiantScolarite.scolarite_semestre à l'etudiant pour l'afficher dans le tableau
-        etudiants.value.forEach(etudiant => {
-          etudiant.semestres = etudiant.etudiantScolarite.scolarite_semestre.map(es => es.structure_semestre)
-        })
+  etudiants.value.forEach(etudiant => {
+    etudiant.etudiantScolarite = etudiant.etudiantScolarites.find(es => es.structureAnneeUniversitaire.id === selectedAnneeUniv.value.id)
+  })
+  etudiants.value.forEach(etudiant => {
+    etudiant.semestres = etudiant.etudiantScolarite.scolarite_semestre.map(es => es.structure_semestre)
+  })
 
-        console.log(etudiants.value)
-      }
+  console.log(etudiants.value)
+}
 
-      onMounted(async () => {
-        if (usersStore.departementDefaut) {
-          departementId.value = usersStore.departementDefaut.id;
-          await getAnneesUniv();
-          await loadEtudiants();
-        } else {
-          console.error('departementDefaut is not defined');
-        }
-      })
+onMounted(async () => {
+  if (usersStore.departementDefaut) {
+    departementId.value = usersStore.departementDefaut.id;
+    await getAnneesUniv();
+  } else {
+    console.error('departementDefaut is not defined');
+  }
+})
 
-      const onPageChange = async (event) => {
-        page.value = event.page
-        limit.value = event.rows
-        await loadEtudiants();
-      }
+const onPageChange = async (event) => {
+  page.value = event.page
+  limit.value = event.rows
+  await loadEtudiants();
+}
 
-      const viewEtudiant = (etudiant) => {
-        selectedEtudiant.value = etudiant
-        showViewDialog.value = true
-      }
+const viewEtudiant = (etudiant) => {
+  selectedEtudiant.value = etudiant
+  showViewDialog.value = true
+}
 
-      const editEtudiant = (etudiant) => {
-        selectedEtudiant.value = etudiant
-        showEditDialog.value = true
-      }
+const editEtudiant = (etudiant) => {
+  selectedEtudiant.value = etudiant
+  showEditDialog.value = true
+}
 
-      const deleteEtudiant = (etudiant) => {
-        console.log(etudiant)
-      }
+const deleteEtudiant = (etudiant) => {
+  console.log(etudiant)
+}
 
-      const editAccessEtudiant = (etudiant) => {
-        selectedEtudiant.value = etudiant
-        showAccessEditDialog.value = true
-      }
+let debounceTimeout;
+watch([filters, selectedAnneeUniv], async (newFilters, newSelectedAnneeUniv) => {
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(async () => {
+    if (newFilters || newSelectedAnneeUniv) {
+      await loadEtudiants();
+    }
+  }, 300);
+})
 
-      //wtach filters
-      watch([filters, selectedAnneeUniv], async () => {
-        await loadEtudiants();
-      })
-
-      </script>
+</script>
 
       <template>
         <div class="card">
@@ -153,8 +149,8 @@
                 </IconField>
               </div>
             </template>
-            <template #empty> No customers found.</template>
-            <template #loading> Loading customers data. Please wait.</template>
+            <template #empty>Aucun étudiant trouvé</template>
+            <template #loading> Chargement des données des étudiants</template>
             <Column field="nom" :showFilterMenu="false" header="Nom" style="min-width: 12rem">
               <template #body="{ data }">
                 {{ data.nom }}
@@ -187,9 +183,6 @@
             <Column :showFilterMenu="false" style="min-width: 12rem">
               <template #body="slotProps">
                 <ButtonInfo tooltip="Voir les détails" @click="viewEtudiant(slotProps.data)"/>
-                <Button icon="pi pi-key"
-                        v-tooltip.bottom="'Gérer les droits'"
-                        outlined severity="warn" rounded class="mr-2" @click="editAccessEtudiant(slotProps.data)"/>
                 <ButtonEdit
                     tooltip="Modifier le personnel"
                     @click="editEtudiant(slotProps.data)"/>
@@ -204,15 +197,15 @@
 
           <ViewEtudiantDialog
               :isVisible="showViewDialog"
-              :personnel="selectedEtudiant"
+              :etudiant="selectedEtudiant"
               @update:visible="showViewDialog = $event"/>
           <EditEtudiantDialog
               :isVisible="showEditDialog"
-              :personnel="selectedEtudiant"
+              :etudiant="selectedEtudiant"
               @update:visible="showEditDialog = $event"/>
           <AccessEtudiantDialog
               :isVisible="showAccessEditDialog"
-              :personnel="selectedEtudiant"
+              :etudiant="selectedEtudiant"
               @update:visible="showAccessEditDialog = $event"/>
         </div>
       </template>
