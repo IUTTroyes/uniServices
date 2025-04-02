6780988c3cb8f844e74d44d6ff24cbfd4c1dcabe
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-04-02 10:55:01 +0200

    wip: test new previ semestre (demo geii)

--- front/apps/intranet/src/views/Previsionnel/PrevisionnelEnseignementView.vue
+++ front/apps/intranet/src/views/Previsionnel/PrevisionnelEnseignementView.vue
@@ -26,6 +26,8 @@ const isLoadingPrevisionnel = ref(true);
 
 const previSemestreMatiere = ref(null);
 
+const isEditing = ref(false);
+
 const size = ref({ label: 'Petit', value: 'small' });
 const sizeOptions = ref([
   { label: 'Petit', value: 'small' },
@@ -115,6 +117,14 @@ watch([selectedSemestre, selectedAnneeUniv, selectedEnseignement], async ([newSe
 });
 watch(selectedSemestre, async (newSemestre) => {
   await getEnseignements();
+});
+
+watch(isEditing, async (newIsEditing) => {
+  if (!newIsEditing) {
+    isLoadingPrevisionnel.value === true;
+    await getPrevi(selectedSemestre.value.id, selectedAnneeUniv.value.id);
+    isLoadingPrevisionnel.value === false;
+  }
 });
 
 const columns = ref([
@@ -224,7 +234,8 @@ const footerCols = computed(() => [
           <label for="anneeUniversitaire">Année universitaire</label>
         </IftaLabel>
       </div>
-      <Button label="Saisir le prévisionnel" icon="pi pi-plus" />
+      <Button v-if="!isEditing" label="Saisir le prévisionnel" icon="pi pi-plus" @click="isEditing = !isEditing" />
+      <Button v-else label="Afficher le prévisionnel" icon="pi pi-eye" @click="isEditing = !isEditing" />
     </div>
     <ListSkeleton v-if="isLoadingPrevisionnel" class="mt-6" />
     <div v-else>
@@ -241,17 +252,32 @@ const footerCols = computed(() => [
         <div class="flex w-full justify-between my-6">
           <SelectButton v-model="size" :options="sizeOptions" optionLabel="label" dataKey="label" />
         </div>
-        <PrevisionnelTable
-            origin="previMatiereSynthese"
-            :columns="columns"
-            :topHeaderCols="topHeaderCols"
-            :additionalRows="additionalRows"
-            :footerCols="footerCols"
-            :data="previSemestreMatiere[0]"
-            :filters="filters"
-            :size="size.value"
-            :headerTitle="`Prévisionnel du semestre ${selectedSemestre?.libelle} pour la matière ${selectedEnseignement.libelle}`"
-            :headerTitlecolspan="1"/>
+        <div v-if="!isEditing">
+          <PrevisionnelTable
+              origin="previMatiereSynthese"
+              :columns="columns"
+              :topHeaderCols="topHeaderCols"
+              :additionalRows="additionalRows"
+              :footerCols="footerCols"
+              :data="previSemestreMatiere[0]"
+              :filters="filters"
+              :size="size.value"
+              :headerTitle="`Prévisionnel du semestre ${selectedSemestre?.libelle} pour la matière ${selectedEnseignement.libelle}`"
+              :headerTitlecolspan="1"/>
+        </div>
+        <div v-else>
+          <PrevisionnelTable
+              origin="previMatiereForm"
+              :columns="columns"
+              :topHeaderCols="topHeaderCols"
+              :additionalRows="additionalRows"
+              :footerCols="footerCols"
+              :data="previSemestreMatiere[0]"
+              :filters="filters"
+              :size="size.value"
+              :headerTitle="`Prévisionnel du semestre ${selectedSemestre?.libelle} pour la matière ${selectedEnseignement.libelle}`"
+              :headerTitlecolspan="1"/>
+        </div>
       </div>
       <Message v-else-if="previSemestreMatiere < 1 || previSemestreMatiere[0].length < 1" severity="error" icon="pi pi-times-circle">
         Aucun prévisionnel pour cette année universitaire avec ce semestre et cette matière
