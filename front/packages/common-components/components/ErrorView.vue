474204c6d59804c6bc3b8c86abb8d7c648e28a78
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-05-27 16:48:40 +0200

    wip [Previ]: hrs / enseignant

---
+++ front/packages/common-components/components/ErrorView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="error-container">
+    <img src="@common-images/illu/maintenance.svg" alt="Maintenance" class="maintenance-image" />
+    <h2 class="error-message">Une erreur est survenue, veuillez réessayer plus tard</h2>
+  </div>
+</template>
+
+<script setup>
+// No props or logic needed for this simple component
+</script>
+
+<style scoped>
+.error-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 2rem;
+  text-align: center;
+  height: 100%;
+  width: 100%;
+}
+
+.maintenance-image {
+  max-width: 300px;
+  margin-bottom: 2rem;
+}
+
+.error-message {
+  font-size: 1.5rem;
+  color: #666;
+}
+</style>
