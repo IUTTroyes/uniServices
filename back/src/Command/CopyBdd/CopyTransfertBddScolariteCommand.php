6b7ff132ab9517898ec480e458012c30ecdaf511
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2025-09-10 16:38:56 +0200

    feat: ajout de la récupération et affichage des UE
    Ajout de la méthode `getUe` pour récupérer et afficher les informations des UE dans la vue `ProfilEtudiant`. Intégration de l'affichage des moyennes et décisions des UE, avec gestion des états de chargement et des erreurs. Mise à jour des services de structure pour inclure `getUeService`.

--- back/src/Command/CopyBdd/CopyTransfertBddScolariteCommand.php
+++ back/src/Command/CopyBdd/CopyTransfertBddScolariteCommand.php
@@ -5,10 +5,12 @@ namespace App\Command\CopyBdd;
 use App\Entity\Etudiant\EtudiantScolarite;
 use App\Entity\Etudiant\EtudiantScolariteSemestre;
 use App\Repository\EtudiantRepository;
+use App\Repository\ScolEnseignementRepository;
 use App\Repository\Structure\StructureAnneeUniversitaireRepository;
 use App\Repository\Structure\StructureDepartementRepository;
 use App\Repository\Structure\StructureGroupeRepository;
 use App\Repository\Structure\StructureSemestreRepository;
+use App\Repository\Structure\StructureUeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -33,6 +35,8 @@ class CopyTransfertBddScolariteCommand extends Command
     protected array $tAnneeUniversitaire = [];
     protected array $tSemestres = [];
     protected array $tDepartements = [];
+    protected array $tMatieres = [];
+    protected array $tUes = [];
 
     protected array $tGroupes = [];
     protected string $base_url;
@@ -49,6 +53,8 @@ class CopyTransfertBddScolariteCommand extends Command
         protected HttpClientInterface         $httpClient,
         StructureDepartementRepository $structureDepartementRepository,
         StructureGroupeRepository $structureGroupeRepository,
+        ScolEnseignementRepository $scolEnseignementRepository,
+        StructureUeRepository $structureUeRepository,
         ParameterBagInterface                 $params
     )
     {
@@ -59,6 +65,8 @@ class CopyTransfertBddScolariteCommand extends Command
         $this->tEtudiants = $etudiantRepository->findAllByOldIdArray();
         $this->tDepartements = $structureDepartementRepository->findAllByIdArray();
         $this->tGroupes = $structureGroupeRepository->findAllByOldIdArray();
+        $this->tMatieres = $scolEnseignementRepository->findAllByOldIdArray();
+        $this->tUes = $structureUeRepository->findAllByOldIdArray();
         $this->base_url = $params->get('URL_INTRANET_V3');
         $this->httpClient = HttpClient::create([
             'verify_peer' => false,
@@ -105,6 +113,7 @@ FOREIGN_KEY_CHECKS=1');
 
     private function addEtudiantScolarite(): void
     {
+        ini_set('memory_limit', '2G');
         $sql = 'SELECT * FROM etudiant WHERE semestre_id IS NOT NULL and annee_sortie = 0';
         $etudiants = $this->em->executeQuery($sql)->fetchAllAssociative();
 
@@ -112,50 +121,188 @@ FOREIGN_KEY_CHECKS=1');
             $response = $this->httpClient->request('GET', $this->base_url . '/etudiant/' . $etu['id']);
             $scolarites = json_decode($response->getContent(), true);
 
-            foreach ($scolarites as $scol) {
-                if (!array_key_exists($scol['annee'], $this->tAnneeUniversitaire)) {
-                    continue;
-                }
-
-                $scolarite = new EtudiantScolarite();
-                $scolarite->setUuid(UuidV4::v4());
-                $scolarite->setEtudiant($this->tEtudiants[$etu['id']]);
-                $scolarite->setAnneeUniversitaire($this->tAnneeUniversitaire[$scol['annee']]);
-
-                // Définir les propriétés globales de la scolarité
-                $scolarite->setMoyenne(round($scol['bilan']['moyenne'], 2) ?? 0);
-                $scolarite->setNbAbsences($scol['bilan']['nbAbsences'] ?? 0);
-                $scolarite->setMoyennesMatiere($scol['bilan']['moyennesMatieres'] ?? []);
-                $scolarite->setMoyennesUe($scol['bilan']['moyennesUes'] ?? []);
-                $scolarite->setCommentaire($scol['commentaire'] ?? '');
-
-                $scolarite->setOrdre($scol['ordre'] ?? count($scolarites));
-
-                foreach ($this->tDepartements as $departement) {
-                    if ($departement->getOldId() === $etu['departement_id']) {
-                        $scolarite->setDepartement($departement);
-                        break;
+            if ($scolarites && isset($this->tEtudiants[$etu['id']])) {
+                foreach ($scolarites as $scol) {
+                    if (!array_key_exists($scol['annee'], $this->tAnneeUniversitaire)) {
+                        continue;
                     }
-                }
 
-                // Ajouter les semestres
-                foreach ($scol['semestres'] as $semestre) {
-                    foreach ($this->tSemestres as $semestreDest) {
-                        if ($semestreDest->getOldId() === $semestre['id']) {
-                            $annee = $semestreDest->getAnnee();
-                            $scolarite->addAnnee($annee);
+                    $scolarite = new EtudiantScolarite();
+                    $scolarite->setUuid(UuidV4::v4());
+                    $scolarite->setEtudiant($this->tEtudiants[$etu['id']]);
+                    $scolarite->setAnneeUniversitaire($this->tAnneeUniversitaire[$scol['annee']]);
 
-                            $etudiantScolSemestre = new EtudiantScolariteSemestre();
-                            $etudiantScolSemestre->setScolarite($scolarite);
-                            $etudiantScolSemestre->setSemestre($semestreDest);
-//                            $etudiantScolSemestre->setDecision($semestre['decision'] ?? null);
-//                            $etudiantScolSemestre->setProposition($semestre['proposition'] ?? null);
-                            $this->entityManager->persist($etudiantScolSemestre);
+                    // Définir les propriétés globales de la scolarité
+                    $scolarite->setMoyenne(isset($scol['bilan']['moyenne']) ? round($scol['bilan']['moyenne'], 2) : 0);
+                    $scolarite->setNbAbsences($scol['bilan']['nbAbsences'] ?? 0);
+
+                    // Process moyennesMatieres with correct IDs
+                    $moyennesMatieres = [];
+                    if (isset($scol['bilan']['moyennesMatieres']) && is_array($scol['bilan']['moyennesMatieres'])) {
+                        foreach ($scol['bilan']['moyennesMatieres'] as $oldId => $moyenneMatiere) {
+                            // Remove "matiere_" prefix if present
+                            $cleanOldId = str_replace('matiere_', '', $oldId);
+
+                            // If it's a numeric ID, try to find the corresponding matiere
+                            if (is_numeric($cleanOldId) && isset($this->tMatieres[$cleanOldId])) {
+                                $newId = $this->tMatieres[$cleanOldId]->getId();
+                                $moyennesMatieres[$newId] = $moyenneMatiere;
+                            } else {
+                                // Keep the original ID if we can't find a mapping
+                                $moyennesMatieres[$oldId] = $moyenneMatiere;
+                            }
                         }
                     }
-                }
+                    $scolarite->setMoyennesMatiere($moyennesMatieres);
 
-                $this->entityManager->persist($scolarite);
+                    // Process moyennesUes with correct IDs
+                    $moyennesUes = [];
+                    if (isset($scol['bilan']['moyennesUes']) && is_array($scol['bilan']['moyennesUes'])) {
+                        foreach ($scol['bilan']['moyennesUes'] as $oldId => $moyenneUe) {
+                            // Round moyenne to 2 decimal places
+                            if (isset($moyenneUe['moyenne'])) {
+                                $moyenneUe['moyenne'] = round($moyenneUe['moyenne'], 2);
+
+                                // If decision is empty, calculate it based on the moyenne
+                                if (!isset($moyenneUe['decision']) || $moyenneUe['decision'] === null) {
+                                    $moyenneUe['decision'] = $moyenneUe['moyenne'] >= 10 ? 'V' : 'NV';
+                                }
+                            }
+
+                            // If it's a numeric ID, try to find the corresponding UE
+                            if (is_numeric($oldId) && isset($this->tUes[$oldId])) {
+                                $newId = $this->tUes[$oldId]->getId();
+                                $moyennesUes[$newId] = $moyenneUe;
+                            } else {
+                                // Keep the original ID if we can't find a mapping
+                                $moyennesUes[$oldId] = $moyenneUe;
+                            }
+                        }
+                    }
+                    $scolarite->setMoyennesUe($moyennesUes);
+                    $scolarite->setCommentaire($scol['bilan']['commentaire'] ?? '');
+
+                    // Set decision from bilan data (convert string to boolean if needed)
+                    $decision = $scol['bilan']['decision'] ?? null;
+                    if ($decision === 'V') {
+                        $scolarite->setDecision(true);
+                    } elseif ($decision === null) {
+                        $scolarite->setDecision(null);
+                    } else {
+                        $scolarite->setDecision(false);
+                    }
+
+                    // Set proposition if available in the last semester
+                    if (!empty($scol['semestres'])) {
+                        $lastSemester = end($scol['semestres']);
+                        if (isset($lastSemester['proposition']) && $lastSemester['proposition'] !== null) {
+                            // If the proposition is for the next year (like "DUT"), find the appropriate year
+                            foreach ($this->tAnneeUniversitaire as $annee) {
+                                if ($annee->getLibelle() === $lastSemester['proposition']) {
+                                    $scolarite->setProposition($annee->getAnnee());
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    $scolarite->setOrdre($scol['ordre'] ?? count($scolarites));
+
+                    foreach ($this->tDepartements as $departement) {
+                        if ($departement->getOldId() === $etu['departement_id']) {
+                            $scolarite->setDepartement($departement);
+                            break;
+                        }
+                    }
+
+                    // Ajouter les semestres
+                    foreach ($scol['semestres'] as $semestre) {
+                        foreach ($this->tSemestres as $semestreDest) {
+                            if ($semestreDest->getOldId() === $semestre['id']) {
+                                $annee = $semestreDest->getAnnee();
+                                $scolarite->addAnnee($annee);
+
+                                $etudiantScolSemestre = new EtudiantScolariteSemestre();
+                                $etudiantScolSemestre->setScolarite($scolarite);
+                                $etudiantScolSemestre->setSemestre($semestreDest);
+
+                                // Set decision from semestre data (convert string to boolean if needed)
+                                $decision = $semestre['decision'] ?? null;
+                                if ($decision === 'V') {
+                                    $etudiantScolSemestre->setDecision(true);
+                                } elseif ($decision === null) {
+                                    $etudiantScolSemestre->setDecision(null);
+                                } else {
+                                    $etudiantScolSemestre->setDecision(false);
+                                }
+
+                                // Set proposition if available
+                                if (isset($semestre['proposition']) && $semestre['proposition'] !== null) {
+                                    // Find the appropriate semester for the proposition
+                                    foreach ($this->tSemestres as $propositionSemestre) {
+                                        if ($propositionSemestre->getLibelle() === $semestre['proposition']) {
+                                            $etudiantScolSemestre->setProposition($propositionSemestre);
+                                            break;
+                                        }
+                                    }
+                                }
+
+                                // Set moyenne from semestre data
+                                $etudiantScolSemestre->setMoyenne(isset($semestre['moyenne']) ? round($semestre['moyenne'], 2) : 0);
+
+                                // Set moyennesMatieres and moyennesUes from semestre data
+                                // Process moyennesMatieres with correct IDs
+                                $moyennesMatieres = [];
+                                if (isset($semestre['moyennesMatieres']) && is_array($semestre['moyennesMatieres'])) {
+                                    foreach ($semestre['moyennesMatieres'] as $oldId => $moyenneMatiere) {
+                                        // Remove "matiere_" prefix if present
+                                        $cleanOldId = str_replace('matiere_', '', $oldId);
+
+                                        // If it's a numeric ID, try to find the corresponding matiere
+                                        if (is_numeric($cleanOldId) && isset($this->tMatieres[$cleanOldId])) {
+                                            $newId = $this->tMatieres[$cleanOldId]->getId();
+                                            $moyennesMatieres[$newId] = $moyenneMatiere;
+                                        } else {
+                                            // Keep the original ID if we can't find a mapping
+                                            $moyennesMatieres[$oldId] = $moyenneMatiere;
+                                        }
+                                    }
+                                }
+                                $etudiantScolSemestre->setMoyennesMatiere($moyennesMatieres);
+
+                                // Process moyennesUes with correct IDs
+                                $moyennesUes = [];
+                                if (isset($semestre['moyennesUes']) && is_array($semestre['moyennesUes'])) {
+                                    foreach ($semestre['moyennesUes'] as $oldId => $moyenneUe) {
+                                        // Round moyenne to 2 decimal places
+                                        if (isset($moyenneUe['moyenne'])) {
+                                            $moyenneUe['moyenne'] = round($moyenneUe['moyenne'], 2);
+
+                                            // If decision is empty, calculate it based on the moyenne
+                                            if (!isset($moyenneUe['decision']) || $moyenneUe['decision'] === null) {
+                                                $moyenneUe['decision'] = $moyenneUe['moyenne'] >= 10 ? 'V' : 'NV';
+                                            }
+                                        }
+
+                                        // If it's a numeric ID, try to find the corresponding UE
+                                        if (is_numeric($oldId) && isset($this->tUes[$oldId])) {
+                                            $newId = $this->tUes[$oldId]->getId();
+                                            $moyennesUes[$newId] = $moyenneUe;
+                                        } else {
+                                            // Keep the original ID if we can't find a mapping
+                                            $moyennesUes[$oldId] = $moyenneUe;
+                                        }
+                                    }
+                                }
+                                $etudiantScolSemestre->setMoyennesUe($moyennesUes);
+
+                                $this->entityManager->persist($etudiantScolSemestre);
+                            }
+                        }
+                    }
+
+                    $this->entityManager->persist($scolarite);
+                }
             }
         }
         $this->entityManager->flush();
