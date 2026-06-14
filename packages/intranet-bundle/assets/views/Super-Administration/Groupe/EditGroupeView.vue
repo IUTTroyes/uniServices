7328653b56b57299cf2491843a6e952bd3353fb7
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2026-06-14 10:19:58 +0200

    feat(Intranet): :sparkles: SuperAdmin -> delete a Groupe from a Semestre
    Ajout de la possibilité pour un SuperAdmin de retirer un Groupe d'un Semestre

---
+++ packages/intranet-bundle/assets/views/Super-Administration/Groupe/EditGroupeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref, onMounted, computed} from "vue";
+import { ValidatedInput, validationRules, ErrorView, PermissionGuard, ListSkeleton, Access } from "@components";
+
+const hasError = ref(false)
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-title mb-8">
+      <h1 class="text-2xl font-bold">Nouveau département</h1>
+      <p class="text-muted-color">Créez un nouveau département.</p>
+    </div>
+
+    <ErrorView v-if="hasError"/>
+    <template v-else>
+
+    </template>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
