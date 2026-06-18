f4391b8ee8426dc2e579acbe92aa725c460ee4cb
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2026-06-18 13:44:20 +0200

    wip: refactor dashboard layout

--- packages/intranet-bundle/assets/router/modules/dashboardRoutes.js
+++ packages/intranet-bundle/assets/router/modules/dashboardRoutes.js
@@ -3,6 +3,5 @@ export default [
         path: '',
         component: () => import('@/views/DashboardView.vue'),
         name: 'Dashboard',
-        meta: { breadcrumb: [{ label: 'Dashboard', route: '/'}] },
     },
 ];
--- packages/intranet-bundle/assets/views/DashboardView.vue
+++ packages/intranet-bundle/assets/views/DashboardView.vue
@@ -1,205 +1,21 @@
 <script setup>
-import { ref, onMounted, computed } from "vue";
 import {PermissionGuard} from "@components";
 import { useUsersStore } from "@stores";
-import { formatDateLong } from "@helpers/date.js";
-import { getActualitesService, getAgendaService } from "@requests";
 import DashboardPersonnel from "@/components/Personnel/Dashboard.vue";
 import DashboardEtudiant from "@/components/Etudiant/Dashboard.vue";
-import EdtJour from "@/components/Edt/EdtJour.vue";
-import {CardSkeleton, ArticleSkeleton} from "@components";
-import {ErrorView} from "@components";
 
 const userStore = useUsersStore();
-const initiales = ref("");
-const absences = ref([]);
-const date = new Date();
-const actuEvents = ref([]);
-const agendaEvents = ref([]);
-const isLoadingActu = ref(true);
-const isLoadingAgenda = ref(true);
-const hasErrorRss = ref(false);
-
-const userInitiales = computed(
-    () => userStore.user?.prenom?.charAt(0) + userStore.user?.nom?.charAt(0) || ""
-);
-
-const getActualites = async () => {
-  try {
-    actuEvents.value = await getActualitesService();
-  } catch (error) {
-    hasErrorRss.value = true;
-    console.error("Error fetching actualites:", error);
-  } finally {
-    isLoadingActu.value = false;
-  }
-};
-
-const getAgenda = async () => {
-  try {
-    agendaEvents.value = await getAgendaService();
-  } catch (error) {
-    hasErrorRss.value = true;
-    console.error("Error fetching agenda:", error);
-  } finally {
-    isLoadingAgenda.value = false;
-  }
-};
-
-const generateMockAbsences = () => [
-  { semestre: "S1", etudiant: "John Doe", heure: "08:00", prof: "Jane Doe", matiere: "Maths" },
-  { semestre: "S1", etudiant: "John Doe", heure: "08:00", prof: "Jane Doe", matiere: "Maths" },
-  { semestre: "S1", etudiant: "John Doe", heure: "08:00", prof: "Jane Doe", matiere: "Maths" },
-  { semestre: "S1", etudiant: "John Doe", heure: "08:00", prof: "Jane Doe", matiere: "Maths" }
-];
-
-onMounted(async () => {
-  await Promise.all([getActualites(), getAgenda()]);
-  initiales.value = userInitiales.value;
-  absences.value = generateMockAbsences();
-
-  console.log(userStore.userType)
-});
-
-const redirectTo = (link) => {
-  window.open(link, "_blank");
-};
 </script>
 
 <template>
   <div>
-    <div class="m-5 mb-10">
-      <div class="w-1/2" v-if="userStore.isLoading">
-        <ArticleSkeleton/>
-      </div>
-      <div v-else class="flex items-center justify-between">
-        <div class="flex items-center">
-          <div class="w-20 bg-violet-400 rounded-full flex items-center justify-center">
-            <template v-if="userStore.userPhoto">
-              <img :src="userStore.userPhoto" alt="photo de profil" class="rounded-full" />
-            </template>
-            <template v-else>
-              <span class="text-gray-700 text-xl">{{ initiales }}</span>
-            </template>
-          </div>
-          <div class="ml-4">
-            <h2 class="text-2xl! mb-0! font-bold flex items-end gap-2"><span class="font-light">Bonjour,</span> {{ userStore.user.prenom }}</h2>
-
-            <small>{{ formatDateLong(date) }}</small>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="card">
-      <div class="card-title mb-4">
-        <div class="font-semibold text-xl">Aujourd'hui</div>
-      </div>
-      <div class="card-content flex flex-col gap-8">
-        <div>
-          <div class="text-lg text-muted-color font-semibold mb-2">Mes cours à venir</div>
-          <EdtJour />
-        </div>
-        <PermissionGuard permission="isAssistant">
-          <hr>
-          <div class="absences flex flex-col gap-2">
-            <div class="text-lg text-muted-color font-semibold">Suivi des absences entre {{ new Date().getHours() < 13 ? '8h00 et 13h00' : '13h00 et 21h00' }}</div>
-
-            <Message severity="info" icon="pi pi-info-circle">{{ absences.length }} absent.s sur la demie journée en cours.</Message>
-
-            <DataTable :value="absences" paginator :rows="5" :rowsPerPageOptions="[5, 10, 20, 50]" stripedRows showGridlines tableStyle="min-width: 50rem"
-                       paginatorTemplate="RowsPerPageDropdown FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink"
-                       currentPageReportTemplate="{first} to {last} of {totalRecords}">
-              <template #paginatorstart>
-                <Button type="button" icon="pi pi-refresh" text />
-              </template>
-              <template #paginatorend>
-                <Button type="button" icon="pi pi-download" text />
-              </template>
-              <Column field="semestre" header="Semestre" sortable style="width: 25%"></Column>
-              <Column field="etudiant" header="Etudiant" sortable style="width: 25%"></Column>
-              <Column field="heure" header="Heure" sortable style="width: 25%"></Column>
-              <Column field="prof" header="Prof." sortable style="width: 25%"></Column>
-              <Column field="matiere" header="Matière" sortable style="width: 25%"></Column>
-            </DataTable>
-          </div>
-        </PermissionGuard>
-      </div>
-    </div>
-
-    <div class="card h-full">
-      <div class="card-title mb-4">
-        <div class="font-semibold text-xl">Agenda de l'IUT</div>
-        <em>Les évènements à venir</em>
-      </div>
-      <ErrorView v-if="hasErrorRss" />
-      <div v-else>
-        <div class="card-content flex justify-between gap-10 mb-8">
-          <div v-if="isLoadingAgenda" class="flex justify-between w-full gap-10">
-            <ArticleSkeleton />
-            <ArticleSkeleton />
-            <ArticleSkeleton />
-            <ArticleSkeleton />
-          </div>
-          <Message v-else-if="agendaEvents.length === 0" severity="info" icon="pi pi-info-circle" class="w-full">Aucun évènement à venir.</Message>
-          <div v-else v-for="(event, index) in agendaEvents" class="p-4 rounded-md flex-1 flex flex-col justify-between gap-2">
-            <div class="flex flex-col gap-2 items-start">
-              <div>
-                <div class="font-bold text-lg">
-                  {{ event.title.length > 50 ? event.title.slice(0, 50) + '...' : event.title }}
-                </div>
-                <div class="text-sm text-muted-color">
-                  {{ event.date }}
-                </div>
-              </div>
-              <div>
-                <p>
-                  {{ event.content }}
-                </p>
-              </div>
-            </div>
-            <Button class="bg-primary-light" label="En savoir plus" icon="pi pi-external-link" iconPos="right" @click="redirectTo(event.link)"/>
-          </div>
-        </div>
-
-        <div class="card-title mb-4">
-          <div class="font-semibold text-xl">Actualités</div>
-          <em>Les évènements passés</em>
-        </div>
-        <div class="flex justify-between gap-10">
-          <div v-if="isLoadingActu" class="flex justify-between w-full gap-10">
-            <CardSkeleton />
-            <CardSkeleton />
-            <CardSkeleton />
-            <CardSkeleton />
-          </div>
-          <Message v-else-if="actuEvents.length === 0" severity="info" icon="pi pi-info-circle" class="w-full">Aucune actualité.</Message>
-          <Card v-else v-for="(event, index) in actuEvents" :key="index" style="width: 25rem" class="card-actus flex flex-col justify-between overflow-hidden w-25">
-            <template #header>
-              <div class="h-32 w-full overflow-hidden flex items-center">
-                <img :alt="event.title" :src="event.image" />
-              </div>
-              <div class="text-lg font-bold px-5 pt-3">{{ event.title.length > 50 ? event.title.slice(0, 50) + '...' : event.title }}</div>
-              <div class="text-sm px-5 text-muted-color">{{event.date}}</div>
-            </template>
-            <template #content>
-              <div class="flex flex-col justify-between gap-2">
-                <div>{{event.content}}</div>
-                <Button label="En savoir plus" icon="pi pi-external-link" iconPos="right" class="bg-primary-light" @click="redirectTo(event.link)"/>
-              </div>
-            </template>
-          </Card>
-        </div>
-      </div>
-    </div>
-
-    <PermissionGuard permission="isPersonnel">
-      <DashboardPersonnel v-if="userStore.isPersonnel"/>
-    </PermissionGuard>
-    <PermissionGuard permission="isEtudiant">
-      <DashboardEtudiant v-if="userStore.isEtudiant" />
-    </PermissionGuard>
-  </div>
+  <PermissionGuard permission="isPersonnel">
+    <DashboardPersonnel v-if="userStore.isPersonnel"/>
+  </PermissionGuard>
+  <PermissionGuard permission="isEtudiant">
+    <DashboardEtudiant v-if="userStore.isEtudiant" />
+  </PermissionGuard>
+</div>
 </template>
 
 <style scoped>
