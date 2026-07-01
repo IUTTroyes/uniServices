117fe6bf51e13ff5b491e174f29798c8c0300252
CyndelHerolt <cyndel.herolt@univ-reims.fr>
2026-07-01 13:34:37 +0200

    feat: add editing mode to WidgetCard and update API base URL to 127.0.0.1

--- shared/components/components/Dashboard/WidgetCard.vue
+++ shared/components/components/Dashboard/WidgetCard.vue
@@ -1,5 +1,7 @@
 <script setup>
-import { computed, defineProps } from 'vue';
+import { computed, defineProps, ref } from 'vue';
+
+const isEditing = ref(false);
 
 const props = defineProps({
     widget: {
@@ -61,19 +63,26 @@ const toggleWidget = () => {
     :style="gridStyle"
     class="card m-0! lg:p-6! p-4!"
     >
+    <div v-if="isEditing">
+        <div class="flex justify-between">
+            <div class="flex items-center gap-1">
+                <Button icon="pi pi-plus" size="small" text rounded title="Augmenter largeur" @click="increaseColSpan"/>
+                <Button icon="pi pi-minus" size="small" text rounded title="Diminuer largeur" @click="decreaseColSpan"/>
+                <Button icon="pi pi-chevron-down" size="small" text rounded title="Augmenter hauteur" @click="increaseRowSpan"/>
+                <Button icon="pi pi-chevron-up" size="small" text rounded title="Diminuer hauteur" @click="decreaseRowSpan"/>
+                <Button v-if="!first" icon="pi pi-arrow-left" size="small" title="Déplacer sur la gauche" text rounded severity="secondary" @click="moveWidget(-1)"/>
+                <Button v-if="!last" icon="pi pi-arrow-right" size="small" title="Déplacer sur la droite" text rounded severity="secondary" @click="moveWidget(1)"/>
+                <Button icon="pi pi-trash" size="small" title="Retirer le widget" text rounded severity="danger" @click="toggleWidget()"/>
+            </div>
+            <Button icon="pi pi-times" size="small" text rounded title="Configurer le widget" @click="isEditing = !isEditing"/>
+        </div>
+        <Divider/>
+    </div>
     <div class="mb-3 flex items-start justify-between gap-2">
         <div class="font-semibold text-xl"><i :class="`${widget.icon} mr-2 text-primary-500`"/>{{ widget.label }}</div>
-        <div class="flex items-center gap-1">
-            <Button icon="pi pi-plus" size="small" text rounded title="Augmenter largeur" @click="increaseColSpan"/>
-            <Button icon="pi pi-minus" size="small" text rounded title="Diminuer largeur" @click="decreaseColSpan"/>
-            <Button icon="pi pi-chevron-down" size="small" text rounded title="Augmenter hauteur" @click="increaseRowSpan"/>
-            <Button icon="pi pi-chevron-up" size="small" text rounded title="Diminuer hauteur" @click="decreaseRowSpan"/>
-            <Button v-if="!first" icon="pi pi-arrow-left" size="small" text rounded severity="secondary" @click="moveWidget(-1)"/>
-            <Button v-if="!last" icon="pi pi-arrow-right" size="small" text rounded severity="secondary" @click="moveWidget(1)"/>
-            <Button icon="pi pi-times" size="small" text rounded severity="danger" @click="toggleWidget()"/>
-        </div>
+        <Button v-if="!isEditing" icon="pi pi-cog" size="small" text rounded title="Configurer le widget" @click="isEditing = !isEditing"/>
     </div>
-    <div class="text-sm text-color-secondary mb-2">{{ widget.code }}</div>
-    <div class="widget-data">{{ data[widget.code] || { message: 'Chargement...' } }}</div>
+    <div class="text-sm text-color-secondary mb-2">{{ widget }}</div>
+    <div class="widget-data"></div>
 </article>
 </template>
