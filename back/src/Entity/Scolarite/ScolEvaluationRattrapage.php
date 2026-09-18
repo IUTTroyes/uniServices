45ba18e29c0b15d69ac3c05853b2a4ea456316af
David <david.annebicque@gmail.com>
2026-09-18 17:06:21 +0200

    refactor(doctrine): remove obsolete lifecycle callbacks

--- back/src/Entity/Scolarite/ScolEvaluationRattrapage.php
+++ back/src/Entity/Scolarite/ScolEvaluationRattrapage.php
@@ -13,7 +13,6 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ScolEvaluationRattrapageRepository::class)]
-#[ORM\HasLifecycleCallbacks]
 #[ApiResource]
 class ScolEvaluationRattrapage
 {
