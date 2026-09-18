8470786c7ccd53a9455d868affed99461aa01267
David <david.annebicque@gmail.com>
2026-09-18 17:09:02 +0200

    refactor(doctrine): remove unused lifecycle callback markers

--- packages/intranet-bundle/src/Entity/Etudiant/EtudiantAbsenceJustificatif.php
+++ packages/intranet-bundle/src/Entity/Etudiant/EtudiantAbsenceJustificatif.php
@@ -22,7 +22,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: EtudiantAbsenceJustificatifRepository::class)]
-#[ORM\HasLifecycleCallbacks]
 #[ApiFilter(JustificatifAbsenceFilter::class)]
 #[ApiFilter(OrderFilter::class, properties: [
     'debut',
