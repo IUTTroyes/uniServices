4559be58957d3f4a6e716cca919ae20d068560b8
David <david.annebicque@gmail.com>
2026-09-18 17:21:16 +0200

    refactor(doctrine): normalize entity timestamps

--- packages/stage-bundle/src/Entity/Stages/StageSoutenance.php
+++ packages/stage-bundle/src/Entity/Stages/StageSoutenance.php
@@ -8,7 +8,8 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
-use App\Entity\Traits\LifeCycleTrait;
+use App\Entity\Contracts\TimestampableInterface;
+use App\Entity\Traits\TimestampableTrait;
 use App\Entity\Users\Personnel;
 use StageBundle\Repository\Stages\StageSoutenanceRepository;
 use Doctrine\DBAL\Types\Types;
@@ -31,9 +32,9 @@ use Symfony\Component\Serializer\Attribute\Groups;
         new Delete()
     ]
 )]
-class StageSoutenance
+class StageSoutenance implements TimestampableInterface
 {
-    use LifeCycleTrait;
+    use TimestampableTrait;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
